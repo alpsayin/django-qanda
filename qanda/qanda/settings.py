4c77f88ec8aebbd54d6785af1674276d009d18a7
Alp Sayin <alpsayin@gmail.com>
2013-01-22 15:17:12 +0200

    change db file name and add qanda_app
    Signed-off-by: Alp Sayin <alpsayin@gmail.com>

--- qanda/qanda/settings.py
+++ qanda/qanda/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'qanda',                      # Or path to database file if using sqlite3.
+        'NAME': 'qanda_sqlite_db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'taggit',
     'south',
+    'qanda_app',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS={
