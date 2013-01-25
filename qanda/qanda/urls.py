dceddf473a987e8a8b6b2df30a010c0e3522841e
Alp Sayin <alpsayin@gmail.com>
2013-01-25 14:23:25 +0200

    change app url

--- qanda/qanda/urls.py
+++ qanda/qanda/urls.py
@@ -15,5 +15,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^question/', include('qanda_app.urls')),
+    url(r'^qanda/', include('qanda_app.urls')),
 )
