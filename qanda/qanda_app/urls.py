377acb1c46513f8098f07c8b690fddd32740d83b
Alp Sayin <alpsayin@gmail.com>
2013-01-25 19:34:42 +0200

    add new question page to urls

--- qanda/qanda_app/urls.py
+++ qanda/qanda_app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls import patterns
-from views import question_page
+from views import question_page, new_question_page
 
 from tastypie.api import Api
 from api import UserResource, QandaUserResource, QuestionResource, AnswerResource, ReplyResource
@@ -14,6 +14,7 @@ v1_api.register(ReplyResource())
 
 urlpatterns = patterns('qanda_app',
     url(r'^$', 'views.index', name='index'),
+    url(r'^new/$', new_question_page, name='new_question_page'),
     url(r'^(?P<question_id>\d+)/$', question_page, name='question_page'),
 
     #api
