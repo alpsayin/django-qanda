a10439c214f4e2b77547ff6307b28391333bf69a
Alp Sayin <alpsayin@gmail.com>
2013-01-25 18:29:08 +0200

    replace relation processing algorithms with functions

--- qanda/qanda_app/views.py
+++ qanda/qanda_app/views.py
@@ -17,68 +17,97 @@ def get_user(request):
 def index(request):
 	return HttpResponse("questions index")
 
+def process_question_relations(request, question, qandaUser):
+	if 'star' in request.POST:
+		Question.objects.star(qandaUser, question)
+	else:
+		Question.objects.unstar(qandaUser, question)
+	if 'flag' in request.POST:
+		Question.objects.flag(qandaUser, question)
+	else:
+		Question.objects.unflag(qandaUser, question)
+	if 'upvote' in request.POST:
+		Question.objects.upvote(qandaUser, question)
+	else:
+		Question.objects.unupvote(qandaUser, question)
+	if 'downvote' in request.POST:
+		Question.objects.downvote(qandaUser, question)
+	else:
+		Question.objects.undownvote(qandaUser, question)
+	if 'useful' in request.POST:
+		Question.objects.useful(qandaUser, question)
+	else:
+		Question.objects.unuseful(qandaUser, question)
+	if 'notUseful' in request.POST:
+		Question.objects.notUseful(qandaUser, question)
+	else:
+		Question.objects.unnotUseful(qandaUser, question)
+
+def process_answer_relations(request, answer, qandaUser):
+	if 'star' in request.POST:
+		Answer.objects.star(qandaUser, answer)
+	else:
+		Answer.objects.unstar(qandaUser, answer)
+	if 'flag' in request.POST:
+		Answer.objects.flag(qandaUser, answer)
+	else:
+		Answer.objects.unflag(qandaUser, answer)
+	if 'upvote' in request.POST:
+		Answer.objects.upvote(qandaUser, answer)
+	else:
+		Answer.objects.unupvote(qandaUser, answer)
+	if 'downvote' in request.POST:
+		Answer.objects.downvote(qandaUser, answer)
+	else:
+		Answer.objects.undownvote(qandaUser, answer)
+	if 'useful' in request.POST:
+		Answer.objects.useful(qandaUser, answer)
+	else:
+		Answer.objects.unuseful(qandaUser, answer)
+	if 'notUseful' in request.POST:
+		Answer.objects.notUseful(qandaUser, answer)
+	else:
+		Answer.objects.unnotUseful(qandaUser, answer)
+
+def process_new_answer(request, question, qandaUser):
+	"""
+		Process a new answer
+	"""
+	# print process_new_answer.__doc__
+	Answer.objects.create_answer(question, qandaUser, request.POST['answer_text'])
+
+def process_new_reply(request, answer, qandaUser):
+	"""
+		Process a new reply
+	"""
+	# print process_new_reply.__doc__
+	Reply.objects.create_reply(answer, qandaUser, request.POST['reply_text'])
+
 def question_page(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
+	user = get_user(request)
+	qandaUser = user.QandaUser
+
 	if request.method == 'POST':
 		if request.POST['type'] == 'question' and request.POST['pk'] == str(question.pk):
-			if 'star' in request.POST:
-				Question.objects.star(get_user(request), question)
-			else:
-				Question.objects.unstar(get_user(request), question)
-			if 'flag' in request.POST:
-				Question.objects.flag(get_user(request), question)
-			else:
-				Question.objects.unflag(get_user(request), question)
-			if 'upvote' in request.POST:
-				Question.objects.upvote(get_user(request), question)
-			else:
-				Question.objects.unupvote(get_user(request), question)
-			if 'downvote' in request.POST:
-				Question.objects.downvote(get_user(request), question)
-			else:
-				Question.objects.undownvote(get_user(request), question)
-			if 'useful' in request.POST:
-				Question.objects.useful(get_user(request), question)
-			else:
-				Question.objects.unuseful(get_user(request), question)
-			if 'notUseful' in request.POST:
-				Question.objects.notUseful(get_user(request), question)
-			else:
-				Question.objects.unnotUseful(get_user(request), question)
+			process_question_relations(request, question, qandaUser)
 		elif request.POST['type'] == 'answer':
-			if 'star' in request.POST:
-				Answer.objects.star(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-			else:
-				Answer.objects.unstar(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-			if 'flag' in request.POST:
-				Answer.objects.flag(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-			else:
-				Answer.objects.unflag(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-			if 'upvote' in request.POST:
-				Answer.objects.upvote(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-			else:
-				Answer.objects.unupvote(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-			if 'downvote' in request.POST:
-				Answer.objects.downvote(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-			else:
-				Answer.objects.undownvote(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-			if 'useful' in request.POST:
-				Answer.objects.useful(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-			else:
-				Answer.objects.unuseful(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-			if 'notUseful' in request.POST:
-				Answer.objects.notUseful(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-			else:
-				Answer.objects.unnotUseful(get_user(request).QandaUser, Answer.objects.get(pk=int(request.POST['pk'])))
-	question = get_object_or_404(Question, pk=question_id)
-	answers = question.answers.all()
-	context = {}
-	context['question'] = question
+			answer = Answer.objects.get(pk=int(request.POST['pk']))
+			process_answer_relations(request, answer, qandaUser)
+		elif request.POST['type'] == 'new_answer':
+			process_new_answer(request, question, qandaUser)
+		elif request.POST['type'] == 'new_reply':
+			answer = Answer.objects.get(pk=int(request.POST['pk']))
+			process_new_reply(request, answer, qandaUser)
+
 	question.relations = QuestionRelatedUsers.objects.filter(relatedQuestion=question, relatedUser__djangoUser=get_user(request))[0]
+	answers = question.answers.filter(deleted=False)
 	for answer in answers:
 		if AnswerRelatedUsers.objects.filter(relatedAnswer=answer, relatedUser__djangoUser=get_user(request)).count():
 			answer.relations = AnswerRelatedUsers.objects.filter(relatedAnswer=answer, relatedUser__djangoUser=get_user(request))[0]
 
+	context = {}
+	context['question'] = question
 	context['answers'] = answers
 	context['debug'] = ''
 	return render_to_response('question.html', context, context_instance=RequestContext(request))
