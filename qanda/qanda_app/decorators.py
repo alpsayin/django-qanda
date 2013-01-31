4af0fe979cde85333ac7719c3d3f213f13ba242d
Alp Sayin <alpsayin@gmail.com>
2013-01-31 16:46:48 +0200

    add new decorator to be used to assure qanda user exists

---
+++ qanda/qanda_app/decorators.py
@@ -0,0 +1,14 @@
+from models import QandaUser
+from django.contrib import auth
+
+def assert_qanda_user(function):
+	"""
+		Check if QandaUser exists for user and create if not
+	"""
+	def _decorated(*args, **kwargs):
+		user = auth.get_user(args[0])
+		if not QandaUser.objects.filter(djangoUser=user).exists():
+			QandaUser.objects.create_user(user)
+		print user.QandaUser
+		return function(*args, **kwargs)
+	return _decorated
