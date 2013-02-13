8dc2667a52eb5da3cbffe0fe647a68c26a30c5b4
Alp Sayin <alpsayin@gmail.com>
2013-02-13 23:07:35 +0200

    add url for last question and question list

--- qanda/qanda_app/urls.py
+++ qanda/qanda_app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls import patterns
-from views import question_page, new_question_page, question_relation_submit, subscription_submit
+from views import question_page, new_question_page, question_relation_submit, subscription_submit, most_recent_question, question_list
 from tastypie.api import Api
 from api import UserResource, QandaUserResource, QuestionResource, AnswerResource, ReplyResource
 from django_notify.urls import get_pattern as get_notify_pattern
@@ -15,6 +15,8 @@ v1_api.register(ReplyResource())
 urlpatterns = patterns('qanda_app',
     url(r'^$', 'views.index', name='index'),
     url(r'^new/$', new_question_page, name='new_question_page'),
+    url(r'^last/$', most_recent_question, name='most_recent_question'),
+    url(r'^list/(?P<question_id>\d+)/$', question_list, name='question_list'),
     url(r'^(?P<question_id>\d+)/$', question_page, name='question_page'),
     url(r'^(?P<question_id>\d+)/relate/$', question_relation_submit, name='question_relation_submit'),
     url(r'^(?P<question_id>\d+)/subscribe/$', subscription_submit, name='subscription_submit'),
