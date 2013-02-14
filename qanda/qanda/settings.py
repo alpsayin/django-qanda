44c02fc82b6f9f1bca5113ea299021894644516f
Alp Sayin <alpsayin@gmail.com>
2013-02-14 20:15:38 +0200

    remove temporary db settings

--- qanda/qanda/settings.py
+++ qanda/qanda/settings.py
@@ -9,17 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#         'NAME': 'inforigo',                      # Or path to database file if using sqlite3.
-#         'USER': 'inforigo',                      # Not used with sqlite3.
-#         'PASSWORD': 'inforigo03011990',                  # Not used with sqlite3.
-#         'HOST': 'kyledbinstance.cyxa6yclbgha.us-east-1.rds.amazonaws.com',                      # Set to empty string for localhost. Not used with sqlite3.
-#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
