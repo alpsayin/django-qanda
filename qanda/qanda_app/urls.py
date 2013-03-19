cb08cee9ae2e7877b8f3d004548e2671f9abc060
Alp Sayin <alpsayin@gmail.com>
2013-03-19 22:33:05 +0200

    add page for editing a question

--- qanda/qanda_app/urls.py
+++ qanda/qanda_app/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls import patterns
 from views import question_page
 from views import new_question_page
+from views import edit_question_page
 from views import question_relation_submit
 from views import subscription_submit
 from views import most_recent_question
@@ -35,6 +36,7 @@ urlpatterns = patterns('qanda_app',
     url(r'^$', 'views.index', name='index'),
     url(r'^qanda-login-redirect/(?P<redirect_url>.+)$', login_redirect, name='login_redirect'),
     url(r'^new/$', new_question_page, name='new_question_page'),
+    url(r'^edit/(?P<question_id>\d+)/$', edit_question_page, name='edit_question_page'),
     url(r'^last/$', most_recent_question, name='most_recent_question'),
     url(r'^list/(?P<question_id>\d+)/$', question_list, {'category' : 'all'}, name='question_list'),
     url(r'^list/(?P<question_id>\d+)/(?P<category>\w+)/$', question_list, name='categorized_question_list'),
