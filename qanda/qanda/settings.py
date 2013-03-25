840204e9d9e3b69c4913458fd59a1cbcdf53d580
Alp Sayin <alpsayin@gmail.com>
2013-03-25 20:37:37 +0200

    re order middlewares and add translation middleware and context processor

--- qanda/qanda/settings.py
+++ qanda/qanda/settings.py
@@ -88,14 +88,15 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # 'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'qanda.urls'
@@ -135,6 +136,7 @@ TEMPLATE_CONTEXT_PROCESSORS={
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.static',
     'django.core.context_processors.request',
+    'django.core.context_processors.i18n',
     'qanda_app.context_processors.search_form',
     'qanda_app.context_processors.latest_questions',
     'qanda_app.context_processors.site_settings',
