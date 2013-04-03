be969c8e70c161583c8f7ac0a574206b93eafe02
Alp Sayin <alpsayin@gmail.com>
2013-04-03 18:15:15 +0300

    new form for closing questions

--- qanda/qanda_app/forms.py
+++ qanda/qanda_app/forms.py
@@ -1,6 +1,8 @@
 from django.forms import ModelForm, Textarea, TextInput, Select
 from models import Question, Answer, Reply
 from django import forms
+from django.conf import settings
+from django.utils.translation import ugettext as _
 
 
 class QuestionForm(ModelForm):
@@ -17,6 +19,9 @@ class QuestionForm(ModelForm):
 			'text': Textarea(attrs={'cols': 120, 'rows': 16, 'class':'span8'}),
 			# 'tags': TextInput(attrs={'size': 220, 'class':'span8'}),
 		}
+
+class QuestionCloseForm(forms.Form):
+	message = forms.CharField(max_length=511, widget=Textarea(attrs={'rows': 10, 'id':'questionCloseForm',}), initial=_("This question is closed by the administrators. ")+getattr(settings, 'ROOT_URL', 'http://127.0.0.1:8000/qanda'))
 
 class AnswerForm(ModelForm):
 	class Meta:
