66f4b34bc9428e2280797fd1ffe86346642d689c
Alp Sayin <alpsayin@gmail.com>
2013-04-04 04:40:44 +0300

    update install required

--- install_required.sh
+++ install_required.sh
@@ -3,10 +3,15 @@
 pip install django
 pip install south
 pip install fabric
-pip install django-taggit
+# pip install django-taggit
+git clone git://github.com/shacker/django-taggit.git
+python django-taggit/setup.py install
 pip install django-notify
 pip install mysql-python
 pip install django-extensions
 pip install werkzeug
 pip install django-tastypie
 pip install django-debug-toolbar
+pip install django-haystack
+pip install django-userena
+pip install pysolr
