56d39da4e35e77d9ee7e04c1d03088a351b93304
Alp Sayin <alpsayin@gmail.com>
2013-04-23 21:20:11 +0300

    add a simple template print debug tag

--- qanda/qanda_app/templatetags/qanda_extras.py
+++ qanda/qanda_app/templatetags/qanda_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
+import pprint
 import re
 
 register = template.Library()
@@ -9,6 +10,15 @@ register = template.Library()
 @stringfilter
 def url_target_blank(text):
 	return mark_safe(re.sub("<a([^>]+)(?<!target=)>",'<a target="_blank"\\1>', text))
+
+
+def template_print(value):
+	print '>>>>>>>>>>>TEMPLATE_DEBUG_PRINT<<<<<<<<<<<<<<'
+	pprint.pprint(value)
+	print '>>>>>>>>>>>TEMPLATE_DEBUG_PRINT<<<<<<<<<<<<<<'
+	return u''
+
+register.simple_tag(template_print)
 
 # url_target_blank.is_safe = True
 # url_target_blank = register.filter(url_target_blank)
